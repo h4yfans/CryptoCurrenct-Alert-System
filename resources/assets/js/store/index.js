640219571cf2c5a71cfbd4442f322dbac25f8b48
Kaan <kaan94karaca@gmail.com>
2017-12-06 20:35:19 +0300

    - Added fake alerts data

--- resources/assets/js/store/index.js
+++ resources/assets/js/store/index.js
@@ -7,6 +7,17 @@ Vue.use(VueResource)
 
 export const store = new Vuex.Store({
     state: {
+        alerts: [
+            {id: 1, currency: 'BTC/USD', exchange: 'Bittrex', price: '1200 '},
+            {id: 2, currency: 'BTC/ETH', exchange: 'Bittrex', price: '0.0000065'},
+            {id: 3, currency: 'BTC/XRP', exchange: 'Bittrex', price: '0.00054 '},
+            {id: 4, currency: 'BTC/NEO', exchange: 'Bittrex', price: '0.0123658 '},
+            {id: 5, currency: 'BTC/OMG', exchange: 'Bittrex', price: '0.1512564 '},
+            {id: 6, currency: 'BTC/STRAT', exchange: 'Bittrex', price: '0.00012 '},
+            {id: 7, currency: 'BTC/XLM', exchange: 'Bittrex', price: '0.004412 '},
+            {id: 8, currency: 'BTC/BCC', exchange: 'Bittrex', price: '0.313412 '},
+            {id: 9, currency: 'BTC/VIA', exchange: 'Bittrex', price: '0.499312 '},
+        ],
         isAuth: null, //check if the user is auth
         email: '', //user email
         showNotification: false, //show the alert section in setting component
