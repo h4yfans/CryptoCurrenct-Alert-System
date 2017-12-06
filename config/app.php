f36be851e895b2a0ae1b81f541031052532cf333
Kaan <kaan94karaca@gmail.com>
2017-12-06 20:15:06 +0300

    - Cors kaldırıldı

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
